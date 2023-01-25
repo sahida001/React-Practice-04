9cd041e942ed4d2a598188e02f391bd058383901
sahida-akter <mssahidasmrity@gmail.com>
2023-01-25 23:04:33 +0600

    upload done

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import Card from './Components/Card'
+
+function App() {
+  return ( <div>
+    <h1 className='headingStyle'>TODO APP</h1>
+    <Card/>
+    <Card />
+    <Card />
+   
+ </div>
+  ); 
+}
+
+export default App;
